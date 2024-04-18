50ddb9c2dc31b385471a1eac081d75138f66d6a2
RobSchram <R.F.schram25@gmail.com>
2024-04-18 11:59:22 +0200

    cell data
    store cell data
    get cell data

---
+++ DataLayer/Dao/CellDao.cs
@@ -0,0 +1,48 @@
+﻿using Minesweeper.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicLayer.Dto;
+
+namespace DataLayer.Dao
+{
+    internal class CellDao
+    {
+        public CellDto GetCell(int horizontal, int vertical)
+        {
+            CellDto cell;
+            var query = "Select* FROM cell WHERE x =@horizontal AND y = @vertical";
+            DatabaseConnection databaseConnection = new DatabaseConnection();
+            databaseConnection.OpenConnection();
+            MySqlCommand cmd = new MySqlCommand(query);
+            try
+            {
+                cmd.Parameters.AddWithValue("@horizontal", horizontal);
+                cmd.Parameters.AddWithValue("@vertical", vertical);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                
+                while (reader.Read())
+                {
+                    cell = new CellDto()
+                    {
+                        horizontal = (int)reader["x"],
+                        vertical = (int)reader["y"],
+                        isMine = (int)reader["isMine"],
+                        isVisible = (int)reader["isvisible"],
+                        amountOfMinesAroundCell = (int)reader["amountOfMinesAroundCell"]
+                    };
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return cell;
+        }
+    }
+}
---
+++ DataLayer/StoreData.cs
@@ -0,0 +1,17 @@
+﻿using Minesweeper.Data;
+using LogicLayer.interfaces;
+namespace DataLayer
+{
+    public class StoreData : IStoreData
+    {
+        public void StoreCell(int horizontal, int vertical,int id, int isMine, int isVisible, int amountOfMinesAroundCell)
+        {
+            DatabaseConnection databaseConnection = new DatabaseConnection();
+            databaseConnection.OpenConnection();
+            string query = $"INSERT INTO Cells (horizontal, vertical, IsMine, IsVisible, AmountOfMinesAround) VALUES ({horizontal}, {vertical}, {isMine}, {isVisible}, {amountOfMinesAroundCell})";
+            databaseConnection.ExecuteQuery(query);
+
+        }
+    }
+}
+
